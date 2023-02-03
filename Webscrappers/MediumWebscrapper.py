76e6077d4ac48d3512d89ea7690eac79d7bb3451
Yaksh Shah <capcool79@gmail.com>
2023-02-03 09:44:18 +0530

    Medium Webscrapper

--- Webscrappers/MediumWebscrapper.py
+++ Webscrappers/MediumWebscrapper.py
@@ -13,33 +13,40 @@ trending_url = f'https://medium.com/tag/{tag}'
 latest_url = f'https://medium.com/tag/{tag}/latest'
 driver.get(trending_url)
 time.sleep(2)
-SCROLL_PAUSE_TIME = 0.5
+last_height = driver.execute_script("return document.body.scrollHeight")
+SCROLL_PAUSE_TIME = 3
+while True:
+    # Scroll down to bottom
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    # Wait to load page
+    time.sleep(SCROLL_PAUSE_TIME)
+    # Calculate new scroll height and compare with last scroll height
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        break
+    last_height = new_height
+
 
 # Get scroll height
 last_height = driver.execute_script("return document.body.scrollHeight")
-authors = []
-topics = []
-content_link=[]
-content=[]
-title=[]
+blogs=[]
+blog={}
 
 source_page = driver.page_source
-# articles = driver.find_element('article').text
 soup = BeautifulSoup(source_page, 'html.parser')
 All_Articals = soup.find_all('article')
+
 for article in All_Articals:
-    authors.append(article.find('a', class_="ae af ag ah ai aj ak al am an ao ap aq ar as").find('p').text)
-    content_link.append('https://medium.com'+article.find('div',class_="l",recursive=False).find('a')['href'])
-    content_info=article.find('div',class_="l cj jr").find_all('div')
-    # content_info = article.find('div', class_="l",recursive=False).find_all('div')
-    title.append(content_info[0].text)
-    content.append(content_info[1].text)
-                   # [len(content_info[0].text):])
+    source=article.find('div',recursive=False).find_all('a')
+    blog['authors']=source[0].find('img')['alt']
+    blog['content_link']='https://medium.com'+source[2]['href']
+    content_info=source[3].find_all('div')
+    if(content_info==[]):
+        content_info=source[4].find_all('div')
+    blog['title']=content_info[0].text
+    blog['content']=content_info[1].text
+    blog['image']=source[len(source)-3].find('img')['src']
+    blogs.append(blog)
 
-
-
-    # topics.append(article.select('h2.bd.ij.kc.kd.ke.kf.eo.kg.kh.ki.kj.es.kk.kl.km.kn.ew.ko.kp.kq.kr.fa.ks.kt.ku.kv.fe.ff.fg.fh.fj.fk.bi'))
-print(len(authors))
-print(content_link)
-print(title)
-print(content)
+print(len(blogs))
+print(blogs)
