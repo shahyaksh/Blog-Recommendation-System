c65b56cbc31c5bdd6612cbc40e5b7276917f9654
Shah Yaksh Ajay <74231523+shahyaksh@users.noreply.github.com>
2023-04-03 13:16:40 +0530

    Updated MediumWebscrapper.py

--- Medium/MediumWebscrapper.py
+++ Medium/MediumWebscrapper.py
@@ -5,10 +5,10 @@ from datetime import datetime
 from pytz import timezone
 import json
 All_topics=['Select Topics','cybersecurity','information-security','blockchain','Cryptocurrency','web3','security',
-            'ai','ml','deep-learning','nlp','data-science','data-analysis','image-processing',
+            'ai','machine-learning','deep-learning','nlp','data-science','data-analysis','image-processing',
             'cloud-computing','cloud-services','dev-ops',
             'android','app-development','flutter',
-            'web-development','Software-Development','front-end-development']
+            'web-development','Software-Development','backend-development','backend']
 
 tag=st.selectbox('Choose the topic',All_topics)
 generate_csv=st.button('Generate CSV')
@@ -16,15 +16,14 @@ if tag!='Select Topics':
     blogs=web_scrape(tag)
     st.write(f'Number of Blogs Scrapped are {len(blogs)}')
     # Create Json File
+    st.code(json.dumps(blogs))
     with open('blogs.json','w') as f:
         f.write(json.dumps(blogs))
-    st.write(generate_csv)
     if generate_csv:
         try:
-            st.code(blogs)
             df=pd.read_json('blogs.json')
             time=datetime.now(timezone("Asia/Kolkata")).date()
-            df.to_csv(f'{tag}_mediumblog{time}.csv',index=False)
+            df.to_csv(f'D:/BVM/Sem 6 Work/Mini Project/Blog-Recommendation-System-Notebooks/MediumBlogData/{tag}_mediumblog{time}.csv',index=False)
             st.write('Generated')
         except:
             st.write('Loading....')
