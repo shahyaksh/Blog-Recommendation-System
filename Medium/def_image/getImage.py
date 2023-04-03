66ae8b2981e5337a48543f2ab7b192006de05ebd
Shah Yaksh Ajay <74231523+shahyaksh@users.noreply.github.com>
2023-04-03 13:15:11 +0530

    Update getImage.py

--- Medium/def_image/getImage.py
+++ Medium/def_image/getImage.py
@@ -1,14 +1,14 @@
 cb_security=['cybersecurity','information-security','blockchain','Cryptocurrency','web3','security']
-data_science=['ai','ml','deep-learning','nlp','data-science','data-analysis','image-processing']
+data_science=['ai','machine-learning','deep-learning','nlp','data-science','data-analysis','image-processing']
 cloud_comp=['cloud-computing','cloud-services','dev-ops']
 app_dev=['android','app-development','flutter']
-web_dev=['web-development','Software-Development','front-end-development']
+web_dev=['web-development','software-development','backend-development','backend']
 
-image_src=[{'data-science':'https://d1m75rqqgidzqn.cloudfront.net/wp-data/2019/09/11134058/What-is-data-science-2.jpg'},
-           {'cloud-comp':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhH4Jd0x2eGLltI4LlNIAD0GgQPnpeHJTvcQ&usqp=CAU'},
-            {'app-dev':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTr5C37y_7CIpkmFSGcEBSLrf0WODe3WvhR4A&usqp=CAU'},
-            {'web-dev':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpfK84JnyFZ7TEAQvc-cNMD3W4PXuypt6nNQ&usqp=CAU'},
-            {'cb-security':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZam9qsK_EuHuyP_vHvMpLqkKf6M8MylMlVQ&usqp=CAU'}
+image_src=[{'data-science':'https://miro.medium.com/v2/resize:fill:140:140/1*bkgZjtPIr4lGPHIN0S6ETg.jpeg'},
+           {'cloud-comp':'https://miro.medium.com/v2/resize:fill:140:140/1*EZScbwq2J7pbB6oR4kM8kA.jpeg'},
+            {'app-dev':'https://miro.medium.com/v2/resize:fill:140:140/1*URM9DzJ0YB4mYWv1k61doA.jpeg'},
+            {'web-dev':'https://miro.medium.com/v2/resize:fill:140:140/1*uZxTT58Z7nuOLjWn2EeUhQ.jpeg'},
+            {'cb-security':'https://miro.medium.com/v2/resize:fill:140:140/0*WQ3LjCh_3LN0D3m5'}
            ]
 
 def get_image(tag:str):
