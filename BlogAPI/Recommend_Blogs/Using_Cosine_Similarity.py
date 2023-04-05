c5b596ebb40c4194697460ee5f4f88b05080d8ac
Shah Yaksh Ajay <74231523+shahyaksh@users.noreply.github.com>
2023-04-05 05:38:43 +0530

    Updated Using_Cosine_Similarity.py

--- BlogAPI/Recommend_Blogs/Using_Cosine_Similarity.py
+++ BlogAPI/Recommend_Blogs/Using_Cosine_Similarity.py
@@ -1,17 +1,12 @@
-import mysql.connector as SqlConnector
 import pandas as pd
-import numpy as np
 import nltk
-import re
-import json
-import requests
+import os
+import pathlib
 from nltk import corpus
 from nltk.stem import WordNetLemmatizer
 from nltk.stem import PorterStemmer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-import os
-import pathlib
 
 lst_stopwords=corpus.stopwords.words('english')
 def pre_process_text(text, flg_stemm=False, flg_lemm=True, lst_stopwords=None):
@@ -31,15 +26,24 @@ def pre_process_text(text, flg_stemm=False, flg_lemm=True, lst_stopwords=None):
     return text
 
 def get_similar_blog(blogs:dict,ratings:dict):
+    # Read the blog data
     data_file = os.path.join(pathlib.Path(__file__).parent, "blog_data.csv")
     blogs_df=pd.read_csv(data_file)
+
+    #vectorize the blog content
     count_vec = CountVectorizer()
+
+    #find cosine similarity
     similarity_matrix = count_vec.fit_transform(blogs_df['clean_blog_content'])
     cosine_sim = cosine_similarity(similarity_matrix)
+
+    #get the blogs that are rated 5 by the user
     ratings_df = pd.DataFrame(ratings)
     blogs_to_consider = ratings_df[ratings_df['rating'] == 5]['blog_id']
     high_rated_blogs = blogs_to_consider.values
     rated_blogs = blogs_df[blogs_df['blog_id'].isin(high_rated_blogs)]
+    
+    #get recommended blogs
     recommended_blogs = []
     for blog_id in high_rated_blogs:
         temp_id = blogs_df[blogs_df['blog_id'] == blog_id].index.values[0]
@@ -48,3 +52,6 @@ def get_similar_blog(blogs:dict,ratings:dict):
             if b_id not in recommended_blogs:
                 recommended_blogs.append(b_id)
     return recommended_blogs
+
+
+
